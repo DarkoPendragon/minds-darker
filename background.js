a3cada97d24c450f9f95c0e5418ca956a572524a
DarkoPendragon <darkoodom@gmail.com>
2021-02-21 23:01:29 -0500

    1.0 Update: - Auto updating CSS - Toggle function replaces button click - Manifest permissions changed

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
 chrome.browserAction.onClicked.addListener((tab) => {
-  chrome.tabs.create({active: true, url: "https://www.minds.com/"})
-})
+  chrome.storage.local.get("mindsDarkerSettings", (res) => {
+    if (res.mindsDarkerSettings && res.mindsDarkerSettings == "dark") chrome.tabs.sendMessage(tab.id, "removeMindsDarkerCSS");
+    else if (res.mindsDarkerSettings && res.mindsDarkerSettings == "light") chrome.tabs.sendMessage(tab.id, "setMindsDarkerCSS");
+    else if (!res.mindsDarkerSettings) chrome.tabs.sendMessage(tab.id, "setMindsDarkerData");
+  });
+});
--- content.js
+++ content.js
@@ -1,1 +1,23 @@
+function setCSS() {
+  var link_tag = document.createElement('link');
+  link_tag.setAttribute('rel', 'stylesheet');
+  link_tag.setAttribute('type', 'text/css');
+  link_tag.setAttribute('href', 'https://darkopendragon.github.io/minds-darker/SourceCSS/source.css');
+  link_tag.id = 'minds-darker-id';
+  document.lastChild.appendChild(link_tag);
+}
+setCSS();
+chrome.storage.local.set({"mindsDarkerSettings": "dark"});
 
+chrome.runtime.onMessage.addListener((request, sender, callback) => {
+  if (request == "removeMindsDarkerCSS") {
+    document.getElementById('minds-darker-id').remove();
+    chrome.storage.local.set({"mindsDarkerSettings": "light"});
+  } else if (request == "setMindsDarkerCSS") {
+    chrome.storage.local.set({"mindsDarkerSettings": "dark"});
+    setCSS();
+  } else if (request == "setMindsDarkerData") {
+    chrome.storage.local.set({"mindsDarkerSettings": "dark"});
+    setCSS();
+  }
+});
