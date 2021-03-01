7f2335c6a3b503a59d4009b6d9634a43341ea32b
DarkoPendragon <darkoodom@gmail.com>
2021-03-01 16:28:35 -0500

    update for update checking

--- content.js
+++ content.js
@@ -7,6 +7,27 @@ function setCSS() {
   document.lastChild.appendChild(link_tag);
 }
 
+function checkVersion() {
+  return new Promise((resolve, reject) => {
+    fetch('https://raw.githubusercontent.com/DarkoPendragon/minds-darker/main/manifest.json').then((res) => {
+      res.json().then((res) => {
+        let gitv = res.version
+        let manv = chrome.runtime.getManifest().version
+        resolve(manv == gitv)
+      })
+    }).catch(reject)
+  })
+}
+
+checkVersion().then((r) => {
+  if (!r) {
+    $('.m-v3Topbar__top').append('<div class="updateText"><p>There is a new key update to Minds Darker, update <a href="https://github.com/DarkoPendragon/minds-darker">here.</a></p><button class="updateButton">Close</button></div>');
+    $('.updateButton').on("click", () => {
+      $('.updateText').css('visibility', 'hidden')
+    })
+  }
+})
+
 chrome.storage.local.get("mindsDarkerSettings", (res) => {
   if (res && res.mindsDarkerSettings && res.mindsDarkerSettings == "dark") setCSS();
   else if (res && !res.mindsDarkerSettings) {
@@ -16,14 +37,17 @@ chrome.storage.local.get("mindsDarkerSettings", (res) => {
 })
 
 chrome.runtime.onMessage.addListener((request, sender, callback) => {
-  if (request == "removeMindsDarkerCSS") {
-    document.getElementById('minds-darker-id').remove();
-    chrome.storage.local.set({"mindsDarkerSettings": "light"});
-  } else if (request == "setMindsDarkerCSS") {
-    chrome.storage.local.set({"mindsDarkerSettings": "dark"});
-    setCSS();
-  } else if (request == "setMindsDarkerData") {
-    chrome.storage.local.set({"mindsDarkerSettings": "dark"});
-    setCSS();
+  switch (request) {
+    case "removeMindsDarkerCSS":
+      document.getElementById('minds-darker-id').remove();
+      chrome.storage.local.set({"mindsDarkerSettings": "light"});
+    break;
+    case "setMindsDarkerCSS":
+      chrome.storage.local.set({"mindsDarkerSettings": "dark"});
+      setCSS();
+    break;
+    case "setMindsDarkerData":
+      chrome.storage.local.set({"mindsDarkerSettings": "dark"});
+      setCSS();
   }
 });
